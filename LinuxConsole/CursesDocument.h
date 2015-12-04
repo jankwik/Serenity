ff526229c8622ce598d85d7e7d5e5283c03cf563
Jan Kjetil Wik <jankwik@gmail.com>
2015-12-04 22:43:42 +0100

    *

---
+++ LinuxConsole/CursesDocument.h
@@ -0,0 +1,21 @@
+#ifndef CURSESDOCUMENT_H
+#define CURSESDOCUMENT_H
+
+#include "../Core/Document.h"
+
+namespace Serenity
+{
+	
+	class CursesDocument : public Document
+	{
+
+		public:
+
+			CursesDocument();
+			~CursesDocument();
+
+	};
+
+}
+
+#endif // CURSESDOCUMENT_H
